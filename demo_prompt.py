1d66417d55e602cf565ed92e9a8ded533e387b05
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-17 22:32:21 +0900

    Demo Prompot

--- demo_prompt.py
+++ demo_prompt.py
@@ -12,7 +12,24 @@ def run_demo(session):
     model_id = "anthropic.claude-v2"
     model_id = "anthropic.claude-instant-v1"
 
-    demo_zero_shot(bedrock_runtime, model_id, "What is the diammeter of the sun?")
+    #demo_zero_shot(bedrock_runtime, model_id, "What is the diammeter of the sun?")
+
+    text = """
+    On a given week, the viewers for a TV channel were
+    Monday: 6500 viewers
+    Tuesday: 6400 viewers
+    Wednesday: 6300 viewers
+
+
+    Question: How many viewers can we expect on Friday?
+    Answer: Based on the numbers given and without any more information, there is a daily decrease of 100 viewers. If we assume this trend will continue during the following days, we can expect 6200 viewers on the next day that would be Thursday, and therefore 6100 viewers on the next day that would be Friday.
+
+
+    Question: How many viewers can we expect on Saturday? (Think Step-by-Step)
+    Answer:
+    """
+
+    demo_chain_of_thought(bedrock_runtime, model_id, text)
 
 
 def demo_zero_shot(bedrock_runtime, model_id, text):
@@ -44,3 +61,26 @@ def demo_zero_shot(bedrock_runtime, model_id, text):
     completion_tokens = anthropic_client.count_tokens(completion)
 
     print(f'prompt_tokens: {prompt_tokens:,}, completion_tokens: {completion_tokens:,}')
+
+
+def demo_chain_of_thought(bedrock_runtime, model_id, text):
+
+    print("call demo_chain_of_thought")
+
+    prompt = f"\n\nHuman: {text}\n\nAssistant:"
+
+    request = {
+        "prompt": prompt,
+        "temperature": 0.0,
+        "top_p": 0.5,
+        "top_k": 300,
+        "max_tokens_to_sample": 2048,
+        "stop_sequences": []
+    }
+
+    response = bedrock_runtime.invoke_model(modelId = model_id, body = json.dumps(request))
+
+    response_body_json = json.loads(response["body"].read())
+    completion = response_body_json['completion']
+
+    print(f"Answer: {completion}")
